4f302bf7799a32c2d536e041353a72079074dae3
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-06-14 18:05:58 +0800

    Update dev and stable version
    Update dev and stable version

--- je_mail_thunder/imap/imap_wrapper.py
+++ je_mail_thunder/imap/imap_wrapper.py
@@ -1,14 +1,13 @@
-import sys
 from email import message_from_bytes
 from email import policy
 from email.header import decode_header
 from imaplib import IMAP4_SSL
 from typing import List, Dict, Union
 
-from je_mail_thunder.utils.save_mail_user_content.save_on_env import get_mail_thunder_os_environ
 from je_mail_thunder.utils.exception.exception_tags import mail_thunder_content_login_failed
 from je_mail_thunder.utils.logging.loggin_instance import mail_thunder_logger
 from je_mail_thunder.utils.save_mail_user_content.mail_thunder_content_save import read_output_content
+from je_mail_thunder.utils.save_mail_user_content.save_on_env import get_mail_thunder_os_environ
 
 
 class IMAPWrapper(IMAP4_SSL):
@@ -25,12 +24,15 @@ class IMAPWrapper(IMAP4_SSL):
 
     def imap_later_init(self):
         mail_thunder_logger.info(f"imap_later_init")
-        self.imap_try_to_login_with_env_or_content()
+        try:
+            self.imap_try_to_login_with_env_or_content()
+        except Exception as error:
+            mail_thunder_logger.error(f"imap_later_init, failed: {repr(error)}")
 
     def imap_try_to_login_with_env_or_content(self):
         mail_thunder_logger.info(f"imap_try_to_login_with_env_or_content")
-        user_info = read_output_content()
         try:
+            user_info = read_output_content()
             if user_info is not None and type(user_info) == dict:
                 if user_info.get("user", None) is not None and user_info.get("password", None) is not None:
                     self.login(user_info.get("user"), user_info.get("password"))
@@ -40,68 +42,91 @@ class IMAPWrapper(IMAP4_SSL):
                     if user_info.get("mail_thunder_user", None) is not None and user_info.get(
                             "mail_thunder_user_password", None) is not None:
                         self.login(user_info.get("mail_thunder_user"), user_info.get("mail_thunder_user_password"))
-            return self.login_state
         except Exception as error:
             mail_thunder_logger.info(
                 f"imap_try_to_login_with_env_or_content, "
                 f"failed: {repr(error) + ' ' + mail_thunder_content_login_failed}")
-            return self.login_state
 
     def imap_select_mailbox(self, mailbox: str = "INBOX", readonly: bool = False):
         mail_thunder_logger.info(f"imap_select_mailbox, mailbox: {mailbox}, readonly: {readonly}")
-        select_status = self.select(mailbox=mailbox, readonly=readonly)
-        return True if select_status[0] == "OK" else False
+        try:
+            select_status = self.select(mailbox=mailbox, readonly=readonly)
+            return True if select_status[0] == "OK" else False
+        except Exception as error:
+            mail_thunder_logger.error(
+                f"imap_select_mailbox, mailbox: {mailbox}, readonly: {readonly}, failed: {repr(error)}")
 
     def imap_search_mailbox(self, search_str: [str, list] = "ALL", charset: str = None) -> list:
         mail_thunder_logger.info(f"imap_search_mailbox, search_str: {search_str}, charset: {charset}")
-        response, mail_number_string = self.search(charset, search_str)
-        mail_detail_list = list()
-        for num_of_mail in mail_number_string[0].split():
-            response, mail_data = self.fetch(num_of_mail, "(RFC822)")
-            # [0][1] is message data [0][0] is message decode like RFC822 {565}
-            message = message_from_bytes(mail_data[0][1], policy=policy.default)
-            mail_detail_list.append([response, mail_data[0][0], message])
-        return mail_detail_list
+        try:
+            response, mail_number_string = self.search(charset, search_str)
+            mail_detail_list = list()
+            for num_of_mail in mail_number_string[0].split():
+                response, mail_data = self.fetch(num_of_mail, "(RFC822)")
+                # [0][1] is message data [0][0] is message decode like RFC822 {565}
+                message = message_from_bytes(mail_data[0][1], policy=policy.default)
+                mail_detail_list.append([response, mail_data[0][0], message])
+            return mail_detail_list
+        except Exception as error:
+            mail_thunder_logger.error(
+                f"imap_search_mailbox, search_str: {search_str}, charset: {charset}, failed: {repr(error)}")
 
     def imap_mail_content_list(
             self, search_str: [str, list] = "ALL", charset: str = None) -> List[Dict[str, Union[str, bytes]]]:
         mail_thunder_logger.info(f"imap_mail_content_list, search_str: {search_str}, charset: {charset}")
-        mail_list = self.imap_search_mailbox(search_str, charset)
-        mail_content_dict = dict()
-        mail_content_list = list()
-        for mail_data in mail_list:
-            mail = mail_data[2]
-            mail_content_dict.update({"SUBJECT": mail.get("Subject")})
-            mail_content_dict.update({"FROM": mail.get("FROM")})
-            mail_content_dict.update({"TO": mail.get("TO")})
-            body = ""
-            if mail.is_multipart():
-                for part in mail.get_payload():
-                    body = part.get_payload(decode=False)
-            else:
-                body = mail.get_payload(decode=False)
-            body = str(decode_header(str(body))[0][0])
-            mail_content_dict.update({"BODY": body})
-            mail_content_list.append(mail_content_dict)
+        try:
+            mail_list = self.imap_search_mailbox(search_str, charset)
             mail_content_dict = dict()
-        return mail_content_list
+            mail_content_list = list()
+            for mail_data in mail_list:
+                mail = mail_data[2]
+                mail_content_dict.update({"SUBJECT": mail.get("Subject")})
+                mail_content_dict.update({"FROM": mail.get("FROM")})
+                mail_content_dict.update({"TO": mail.get("TO")})
+                body = ""
+                if mail.is_multipart():
+                    for part in mail.get_payload():
+                        body = part.get_payload(decode=False)
+                else:
+                    body = mail.get_payload(decode=False)
+                body = str(decode_header(str(body))[0][0])
+                mail_content_dict.update({"BODY": body})
+                mail_content_list.append(mail_content_dict)
+                mail_content_dict = dict()
+            return mail_content_list
+        except Exception as error:
+            mail_thunder_logger.error(
+                f"imap_mail_content_list, search_str: {search_str}, charset: {charset}, failed: {repr(error)}")
 
     def imap_output_all_mail_as_file(
             self, search_str: [str, list] = "ALL", charset: str = None) -> List[Dict[str, Union[str, bytes]]]:
         mail_thunder_logger.info(f"imap_mail_content_list, search_str: {search_str}, charset: {charset}")
-        all_mail = self.imap_mail_content_list(search_str=search_str, charset=charset)
-        for mail in all_mail:
-            with open(mail.get("SUBJECT"), "w+") as file:
-                if isinstance(mail.get("BODY"), bytes):
-                    file.write(mail.get("BODY").decode("utf-8"))
+        try:
+            all_mail = self.imap_mail_content_list(search_str=search_str, charset=charset)
+            same_name_dict: Dict[str, int] = dict()
+            for mail in all_mail:
+                if same_name_dict.get((mail.get("SUBJECT"))) is None:
+                    same_name_dict.update({mail.get("SUBJECT"): 0})
                 else:
-                    file.write(mail.get("BODY"))
-        return all_mail
+                    same_name_dict.update({mail.get("SUBJECT"): same_name_dict.get(mail.get("SUBJECT")) + 1})
+                with open(mail.get("SUBJECT") + str(same_name_dict.get(mail.get("SUBJECT"))), "w+") as file:
+                    if isinstance(mail.get("BODY"), bytes):
+                        file.write(mail.get("BODY").decode("utf-8"))
+                    else:
+                        file.write(mail.get("BODY"))
+            return all_mail
+        except Exception as error:
+            mail_thunder_logger.error(
+                f"imap_mail_content_list, search_str: {search_str}, charset: {charset}, failed: {repr(error)}")
 
     def imap_quit(self):
         mail_thunder_logger.info(f"imap_quit")
-        self.close()
-        self.logout()
+        try:
+            self.close()
+            self.logout()
+        except Exception as error:
+            mail_thunder_logger.error(f"imap_quit, failed: {repr(error)}")
 
 
 imap_instance = IMAPWrapper()
+
--- je_mail_thunder/smtp/smtp_wrapper.py
+++ je_mail_thunder/smtp/smtp_wrapper.py
@@ -27,17 +27,26 @@ class SMTPWrapper(SMTP_SSL):
         self.quit()
 
     def smtp_later_init(self):
-        self.smtp_try_to_login_with_env_or_content()
+        mail_thunder_logger.info("smtp_later_init")
+        try:
+            self.smtp_try_to_login_with_env_or_content()
+        except Exception as error:
+            mail_thunder_logger.error(f"smtp_later_init, failed: {error}")
 
     @staticmethod
     def smtp_create_message(message_content: str, message_setting_dict: dict, **kwargs):
         mail_thunder_logger.info(
             f"smtp_create_message, message_content{message_content}, message_setting_dict: {message_setting_dict}")
-        message = EmailMessage(**kwargs)
-        message.set_content(message_content)
-        for key, value in message_setting_dict.items():
-            message[key] = value
-        return message
+        try:
+            message = EmailMessage(**kwargs)
+            message.set_content(message_content)
+            for key, value in message_setting_dict.items():
+                message[key] = value
+            return message
+        except Exception as error:
+            mail_thunder_logger.error(
+                f"smtp_create_message, message_content{message_content}, "
+                f"message_setting_dict: {message_setting_dict}, failed: {repr(error)}")
 
     @staticmethod
     def smtp_create_message_with_attach(message_content: str, message_setting_dict: dict,
@@ -45,61 +54,70 @@ class SMTPWrapper(SMTP_SSL):
         mail_thunder_logger.info(
             f"smtp_create_message_with_attach, message_content{message_content}, "
             f"message_setting_dict: {message_setting_dict}, attach_file: {attach_file}, use_html: {use_html}")
-        message = MIMEMultipart()
-        for key, value in message_setting_dict.items():
-            message[key] = value
-        if use_html:
-            mime_part = MIMEText(message_content, "html")
-        else:
-            mime_part = MIMEText(message_content)
-        message.attach(mime_part)
-        content_type, encoding = guess_type(attach_file)
-        if content_type is None or encoding is not None:
-            content_type = "application/octet-stream"
-        main_type, sub_type = content_type.split("/", 1)
-        if main_type == "text":
-            file_read = open(attach_file, "r+")
-            mime_part = MIMEText(file_read.read(), _subtype=sub_type)
-            file_read.close()
-        elif main_type == "image":
-            file_read = open(attach_file, "rb")
-            mime_part = MIMEImage(file_read.read(), _subtype=sub_type)
-            file_read.close()
-        elif main_type == "audio":
-            file_read = open(attach_file, "rb")
-            mime_part = MIMEAudio(file_read.read(), _subtype=sub_type)
-            file_read.close()
-        else:
-            file_read = open(attach_file, "rb")
-            mime_part = MIMEBase(main_type, sub_type)
-            mime_part.set_payload(file_read.read())
-            file_read.close()
-        filename = path.basename(attach_file)
-        mime_part.add_header("Content-Disposition", "attachment", filename=filename)
-        mime_part.add_header("Content-ID", "{filename}".format(filename=filename))
-        message.attach(mime_part)
-        return message
+        try:
+            message = MIMEMultipart()
+            for key, value in message_setting_dict.items():
+                message[key] = value
+            if use_html:
+                mime_part = MIMEText(message_content, "html")
+            else:
+                mime_part = MIMEText(message_content)
+            message.attach(mime_part)
+            content_type, encoding = guess_type(attach_file)
+            if content_type is None or encoding is not None:
+                content_type = "application/octet-stream"
+            main_type, sub_type = content_type.split("/", 1)
+            if main_type == "text":
+                file_read = open(attach_file, "r+")
+                mime_part = MIMEText(file_read.read(), _subtype=sub_type)
+                file_read.close()
+            elif main_type == "image":
+                file_read = open(attach_file, "rb")
+                mime_part = MIMEImage(file_read.read(), _subtype=sub_type)
+                file_read.close()
+            elif main_type == "audio":
+                file_read = open(attach_file, "rb")
+                mime_part = MIMEAudio(file_read.read(), _subtype=sub_type)
+                file_read.close()
+            else:
+                file_read = open(attach_file, "rb")
+                mime_part = MIMEBase(main_type, sub_type)
+                mime_part.set_payload(file_read.read())
+                file_read.close()
+            filename = path.basename(attach_file)
+            mime_part.add_header("Content-Disposition", "attachment", filename=filename)
+            mime_part.add_header("Content-ID", "{filename}".format(filename=filename))
+            message.attach(mime_part)
+            return message
+        except Exception as error:
+            mail_thunder_logger.info(
+                f"smtp_create_message_with_attach, message_content{message_content}, "
+                f"message_setting_dict: {message_setting_dict}, attach_file: {attach_file}, "
+                f"use_html: {use_html}, failed: {repr(error)}")
 
     def smtp_try_to_login_with_env_or_content(self):
         mail_thunder_logger.info(f"smtp_try_to_login_with_env_or_content")
-        user_info = read_output_content()
-        self.login_state = False
         try:
-            if user_info is not None and type(user_info) == dict:
-                if user_info.get("user", None) is not None and user_info.get("password", None) is not None:
-                    self.login(user_info.get("user"), user_info.get("password"))
-                    self.login_state = True
-            else:
-                user_info = get_mail_thunder_os_environ()
+            user_info = read_output_content()
+            self.login_state = False
+            try:
                 if user_info is not None and type(user_info) == dict:
-                    if user_info.get("mail_thunder_user", None) is not None and user_info.get(
-                            "mail_thunder_user_password", None) is not None:
-                        self.login(user_info.get("mail_thunder_user"), user_info.get("mail_thunder_user_password"))
+                    if user_info.get("user", None) is not None and user_info.get("password", None) is not None:
+                        self.login(user_info.get("user"), user_info.get("password"))
                         self.login_state = True
-            return self.login_state
-        except smtplib.SMTPAuthenticationError as error:
+                else:
+                    user_info = get_mail_thunder_os_environ()
+                    if user_info is not None and type(user_info) == dict:
+                        if user_info.get("mail_thunder_user", None) is not None and user_info.get(
+                                "mail_thunder_user_password", None) is not None:
+                            self.login(user_info.get("mail_thunder_user"), user_info.get("mail_thunder_user_password"))
+                            self.login_state = True
+                return self.login_state
+            except smtplib.SMTPAuthenticationError as error:
+                mail_thunder_logger.info(f"smtp_try_to_login_with_env_or_content, failed: {repr(error)}")
+                return self.login_state
+        except Exception as error:
             mail_thunder_logger.info(f"smtp_try_to_login_with_env_or_content, failed: {repr(error)}")
-            return self.login_state
 
     def quit(self):
         mail_thunder_logger.info(f"SMTP quit")
@@ -110,14 +128,25 @@ class SMTPWrapper(SMTP_SSL):
         mail_thunder_logger.info(
             f"smtp_create_message_with_attach_and_send, message_content: {message_content}, "
             f"message_setting_dict: {message_setting_dict}, attach_file:{attach_file}, use_html:{use_html}")
-        self.send_message(
-            self.smtp_create_message_with_attach(message_content, message_setting_dict, attach_file, use_html))
+        try:
+            self.send_message(
+                self.smtp_create_message_with_attach(message_content, message_setting_dict, attach_file, use_html))
+        except Exception as error:
+            mail_thunder_logger.info(
+                f"smtp_create_message_with_attach_and_send, message_content: {message_content}, "
+                f"message_setting_dict: {message_setting_dict}, attach_file:{attach_file}, "
+                f"use_html:{use_html}, failed: {repr(error)}")
 
     def smtp_create_message_and_send(self, message_content: str, message_setting_dict: dict, **kwargs):
         mail_thunder_logger.info(
             f"smtp_create_message_and_send, message_content: {message_content}, "
             f"message_setting_dict: {message_setting_dict}, params:{kwargs}")
-        self.send_message(self.smtp_create_message(message_content, message_setting_dict, **kwargs))
+        try:
+            self.send_message(self.smtp_create_message(message_content, message_setting_dict, **kwargs))
+        except Exception as error:
+            mail_thunder_logger.info(
+                f"smtp_create_message_and_send, message_content: {message_content}, "
+                f"message_setting_dict: {message_setting_dict}, params:{kwargs}, failed: {repr(error)}")
 
 
 smtp_instance = SMTPWrapper()
