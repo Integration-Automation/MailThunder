a9b1af983d188e6c7382c16b61634bd3aa534a98
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-07-17 17:19:20 +0800

    Revert "Update keyword & dev version"
    This reverts commit 4a27bd810717ae6f67882f680f49b9d06b45f864.

--- je_mail_thunder/utils/project/template/template_keyword.py
+++ je_mail_thunder/utils/project/template/template_keyword.py
@@ -1,23 +1,23 @@
 template_keyword_1: list = [
-    ["MT_smtp_later_init"],
-    ["MT_smtp_create_message_and_send", {
+    ["smtp_later_init"],
+    ["smtp_create_message_and_send", {
         "message_content": "test", "message_setting_dict": {
             "Subject": "test_subject",
             "To": "zenmailman@gmail.com",
             "From": "zenmailman@gmail.com"
         }}
      ],
-    ["MT_smtp_quit"]
+    ["smtp_quit"]
 ]
 
 template_keyword_2: list = [
-    ["MT_imap_later_init"],
-    ["MT_imap_select_mailbox"],
-    ["MT_imap_output_all_mail_as_file"]
+    ["imap_later_init"],
+    ["imap_select_mailbox"],
+    ["imap_output_all_mail_as_file"]
 ]
 
 bad_template_1 = [
-    ["MT_add_package_to_executor", ["os"]],
+    ["add_package_to_executor", ["os"]],
     ["os_system", ["python --version"]],
     ["os_system", ["python -m pip --version"]],
 ]
